b87dc3b201d2daa8cd56b479adfa8618e6989fee
Ítalo Vinícius de Freitas Dias <italo.dias.017@ufrn.edu.br>
2022-03-15 13:46:50 -0300

    Habilitando as rotas do gateway

--- gateway/src/main/java/com/microservices/gateway/RouteLocator.java
+++ gateway/src/main/java/com/microservices/gateway/RouteLocator.java
@@ -4,11 +4,11 @@ import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
 import org.springframework.context.annotation.Bean;
 
 public class RouteLocator {
-    @Bean
-    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
-        return (RouteLocator) builder.routes()
-                .route("microservicesApi", r -> r.path("/api/usuario/login")
-                        .uri("lb://microserviceApi"))
-                .build();
-    }
+//    @Bean
+//    public RouteLocator customRouteLocator(RouteLocatorBuilder builder) {
+//        return (RouteLocator) builder.routes()
+//                .route("microservicesApi", r -> r.path("/api/usuario/login")
+//                        .uri("lb://microserviceApi"))
+//                .build();
+//    }
 }
