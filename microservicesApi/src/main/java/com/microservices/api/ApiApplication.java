3617733a08c5eddebddecf671a76b1d3c9372aa0
Ítalo Vinícius de Freitas Dias <italo.dias.017@ufrn.edu.br>
2022-03-14 21:52:30 -0300

    Creating the gateway microservice and the eureka discovery

--- microservicesApi/src/main/java/com/microservices/api/ApiApplication.java
+++ microservicesApi/src/main/java/com/microservices/api/ApiApplication.java
@@ -3,13 +3,21 @@ package com.microservices.api;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @SpringBootApplication
 @EntityScan({"com.microservices.core.model"})
+@EnableEurekaClient
 @EnableJpaRepositories({"com.microservices.core.repository"})
 public class ApiApplication {
-
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
 	public static void main(String[] args) {
 		SpringApplication.run(ApiApplication.class, args);
 	}
--- microservicesApi/src/main/java/com/microservices/api/config/WebSecurityConfiguration.java
+++ microservicesApi/src/main/java/com/microservices/api/config/WebSecurityConfiguration.java
@@ -1,4 +1,4 @@
-package com.microservices.api.config;
+/*package com.microservices.api.config;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
@@ -25,4 +25,4 @@ public class WebSecurityConfiguration extends WebSecurityConfigurerAdapter {
                 .anyRequest().authenticated();
 
     }
-}
+}*/
